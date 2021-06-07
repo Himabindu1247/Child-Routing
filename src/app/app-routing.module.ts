bdb6a19fad1229442774e1786f894019f7dbbfcf
Himabindu1247 <geethun1247@gmail.com>
2021-06-07 15:27:42 +0530

    Child Routing Example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
+import { AppliancesComponent } from './appliances/appliances.component';
+
+import { MobilesComponent } from './electronics/mobiles/mobiles.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LaptopsComponent } from './electronics/laptops/laptops.component';
+import { OnlineshoppingComponent } from './onlineshopping/onlineshopping.component';
+import { FurnitureComponent } from './appliances/furniture/furniture.component';
+import { LightingComponent } from './appliances/lighting/lighting.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { MenComponent } from './fashion/men/men.component';
+import { WomenComponent } from './fashion/women/women.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component:OnlineshoppingComponent},
+  {path:'electronics' , component:ElectronicsComponent , children:[
+    {path:'mobiles' , component:MobilesComponent},
+    {path:'laptops' , component:LaptopsComponent}
+  ]},
+  {path:'appliances' , component:AppliancesComponent , children:[
+    {path:'furniture' , component:FurnitureComponent},
+    {path:'lighting' , component:LightingComponent}
+  ]},
+  {path:'fashion' , component:FashionComponent , children:[
+    {path:'men' , component:MenComponent},
+    {path:'women' , component:WomenComponent}
+  ]}
+  
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { AppliancesComponent } from './appliances/appliances.component';
+import { OnlineshoppingComponent } from './onlineshopping/onlineshopping.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { LaptopsComponent } from './electronics/laptops/laptops.component';
+import { MobilesComponent } from './electronics/mobiles/mobiles.component';
+import { LightingComponent } from './appliances/lighting/lighting.component';
+import { FurnitureComponent } from './appliances/furniture/furniture.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { MenComponent } from './fashion/men/men.component';
+import { WomenComponent } from './fashion/women/women.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppliancesComponent,
+    OnlineshoppingComponent,
+    ElectronicsComponent,
+    LaptopsComponent,
+    MobilesComponent,
+    LightingComponent,
+    FurnitureComponent,
+    FashionComponent,
+    MenComponent,
+    WomenComponent,
+ 
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
